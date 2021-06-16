a20840d46f031c9b7166949f9fe8ea4fdc1e83e6
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-16 16:36:32 +0900

    Mybatis를 이용한 DB조회
    Mybatis의 장점은 코드가 간결하고 유지보수가 좋다

--- MybatisDB/src/main/java/com/test/dao/userDAO.java
+++ MybatisDB/src/main/java/com/test/dao/userDAO.java
@@ -4,5 +4,5 @@ import java.util.List;
 import com.test.vo.userVO;
 
 public interface userDAO {
-	public List<userVO> selectuser() throws Exception;
+	public List<userVO> selectuser() throws Exception; // Dao에서 Mybatis를 호출하고 VO값을 담아 서비스에 보낸다
 }
